f05487a8e9e6552c3e0f1e2050969488aa947ad7
Mark Kopec <mark.kopec@gmail.com>
2019-02-13 11:38:32 -0700

    Create game class

---
+++ Asteroids/src/game.js
@@ -0,0 +1,16 @@
+function Game() {
+  
+
+}
+
+Game.DIM_X = 500;
+Game.DIM_Y = 300;
+Game.NUM_ASTEROIDS = 5;
+
+Game.prototype.addAsteroidn = function() {
+
+};
+
+Game.prototype.randomPosition = function() {
+
+};
