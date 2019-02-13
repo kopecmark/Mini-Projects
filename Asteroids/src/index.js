18ebedb2ec6ad0759d144aea8fb067b6be538686
Mark Kopec <mark.kopec@gmail.com>
2019-02-13 09:44:56 -0700

    Listen for content loaded

--- Asteroids/src/index.js
+++ Asteroids/src/index.js
@@ -3,3 +3,12 @@ console.log("Webpack is working!");
 const MovingObject = require("./moving_object.js");
 
 window.MovingObject = MovingObject;
+
+document.addEventListener("DOMContentLoaded", function() {
+  const canvasEl = document.getElementById("game-canvas");
+  const ctx = canvasEl.getContext("2d");
+  const mo = new MovingObject(
+    { pos: [30, 30], vel: [10, 10], radius: 20, color: "#00FF00" }
+  );
+  mo.draw(ctx);
+});
