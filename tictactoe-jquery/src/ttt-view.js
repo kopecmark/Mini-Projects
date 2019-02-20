f751bf4715a1e2cf0d3962c132e373647f7b17f0
Mark Kopec <mark.kopec@gmail.com>
2019-02-19 21:22:49 -0700

    Add bind events and make move functions

--- tictactoe-jquery/src/ttt-view.js
+++ tictactoe-jquery/src/ttt-view.js
@@ -3,11 +3,48 @@ class View {
     this.$el = $el;
     this.game = game;
     this.setupBoard();
+    this.bindEvents();
   }
 
-  bindEvents() {}
+  bindEvents() {
+    this.$el.on("click", "li", (event => {
+      const $square = $(event.currentTarget);
+      console.log($square);
+      this.makeMove($square);
+    }));
+  }
 
-  makeMove($square) {}
+  makeMove($square) {
+    const pos = $square.data("pos");
+    const currentPlayer = this.game.currentPlayer;
+
+    try {
+      this.game.playMove(pos);
+    } catch (e) {
+      alert("This " + e.msg.toLowerCase());
+      return;
+    }
+
+    $square.addClass(currentPlayer);
+
+    if (this.game.isOver()) {
+      // cleanup click handlers.
+      this.$el.off("click");
+      this.$el.addClass("game-over");
+
+      const winner = this.game.winner();
+      const $figcaption = $("<figcaption>");
+
+      if (winner) {
+        this.$el.addClass(`winner-${winner}`);
+        $figcaption.html(`You win, ${winner}!`);
+      } else {
+        $figcaption.html("It's a draw!");
+      }
+
+      this.$el.append($figcaption);
+    }
+  }
 
   setupBoard() {
     const $ul = $("<ul>");
