838eb6aeec5ac55686c8ef5a8843eb970b2c4b23
Mark Kopec <mark.kopec@gmail.com>
2019-02-19 21:07:10 -0700

    Create setup board function

--- tictactoe-jquery/src/ttt-view.js
+++ tictactoe-jquery/src/ttt-view.js
@@ -1,11 +1,26 @@
 class View {
-  constructor(game, $el) {}
+  constructor(game, $el) {
+    this.$el = $el;
+    this.game = game;
+    this.setupBoard();
+  }
 
   bindEvents() {}
 
   makeMove($square) {}
 
-  setupBoard() {}
+  setupBoard() {
+    const $ul = $("<ul>");
+
+    for (let rowIdx = 0; rowIdx < 3; rowIdx++) {
+      for (let colIdx = 0; colIdx < 3; colIdx++) {
+        let $li = $("<li>");
+        $ul.append($li);
+      }
+      
+    }
+    this.$el.append($ul);
+  }
 }
 
 module.exports = View;
